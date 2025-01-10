062e61c5dd1e0d877aa074c747bd7e791fda437b
sohanxsahoo <116956354+sohanxsahoo@users.noreply.github.com>
2025-01-10 14:10:58 +0530

    add initial Next.js configuration, styles, and SVG assets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,6 +7,17 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
+  const posts = [{
+    _createdAt: 'Yesterday',
+    views: 55,
+    author: { _id: 1},
+    _id: 1,
+    description: "This is a description",
+    image: "https://images.unsplash.com/photo-1721332150382-d4114ee27eff?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Robots",
+    title: "We Robots"
+  }]
+
   return (
     <>
       <section className="pink_container">
@@ -19,10 +30,14 @@ export default async function Home({
         <SearchForm query={query} />
       </section>
 
-      <section>
-        <p>
-          
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "Startups"}
         </p>
+
+        <ul className="mt-7 card-grid">
+
+        </ul>
       </section>
     </>
   );
